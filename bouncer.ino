61881bd5f60e7ed020259af16f159342f8ae36d7
emmanuelrdl <emmanuel.rudelle@hotmail.fr>
2018-05-03 12:06:22 +0000

    add obstacle struct

--- bouncer.ino
+++ bouncer.ino
@@ -8,6 +8,14 @@
 
 int record = 0;
 
+struct Obstacle {
+  byte height;
+  byte width;
+  byte x;
+  byte y;
+  byte inverse;
+};
+
 
 // La partie Setup concerne ce qui va être exécuté au démarrage de Kitco
 void setup() {
@@ -19,14 +27,56 @@ void setup() {
   record = EEPROM.read(0)+EEPROM.read(1)*255;
 }
 
+// createRectangle will draw a rectangle from x0,y0 top-left corner to
+// a x1,y1 bottom-right corner. Can be filled with the fill
+// parameter, and colored with bw.
+// This function was grabbed from the SparkFun ColorLCDShield
+// library.
+void createRectangle(int x0, int y0, int x1, int y1, boolean fill, boolean bw)
+{
+  // check if the rectangle is to be filled
+  if (fill == 1)
+  {
+    int xDiff;
+
+    if(x0 > x1)
+      xDiff = x0 - x1; //Find the difference between the x vars
+    else
+      xDiff = x1 - x0;
+
+    while(xDiff > 0)
+    {
+      ligneEcran(x0, y0, x0, y1, bw);
+
+      if(x0 > x1)
+        x0--;
+      else
+        x0++;
+
+      xDiff--;
+    }
+  }
+  else
+  {
+    // best way to draw an unfilled rectangle is to draw four lines
+    ligneEcran(x0, y0, x1, y0, bw);
+    ligneEcran(x0, y1, x1, y1, bw);
+    ligneEcran(x0, y0, x0, y1, bw);
+    ligneEcran(x1, y0, x1, y1, bw);
+  }
+}
+
+
+void drawObstacle(struct Obstacle infos) {
+  createRectangle(8, 8, 22, 22, true, NOIR);
+  updateDisplay();
+}
 
 
 void setupGame() {
-  int c = 1;
   clearDisplay(BLANC);
-  // ecrireEcran(s,c,5,NOIR);
-  setChar('c', 10, 10, 1);
-  updateDisplay();
+  struct Obstacle infos  = { 10 , 10 , 0, 0, 0 };
+  drawObstacle(infos);
 }
 
 // Loop est la boucle principale, va se lancer en boucle après Setup
